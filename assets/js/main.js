222dad8b59a72403afbdf41f3626778a30e9818a
magysil <magallysilvab@gmail.com>
2024-02-13 15:55:16 -0300

    Se agregó el jQuery de selectores de etiqueta ySelectores de etiqueta y el método de

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,26 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 
 
 $(document).ready(function(){
+
+    /*Evento “click” de jquery para mostrar una alerta al hacer un clicK
+sobre el botón de "Enviar por Correo" mediante el uso de los selectores por Id*/
     $("#enviarCorreo").click(function(){
       alert("El correo fue enviado correctamente...");
     });
 
+    /*Selectores de etiqueta y el método de jQuery
+`on` con el evento `dblclick`, para modificar el color del texto */
+
+    $("p.ingredientes").on("dblclick", function(){
+        $(this).css("color", "red");
+    });
+
+    $("p.preparacion").on("dblclick", function(){
+        $(this).css("color", "red");        
+    });
+
     
+
+
+
   });
