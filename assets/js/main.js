0aab6af8506265b12e0aa2d609bb22d9af5c5eaa
magysil <magallysilvab@gmail.com>
2024-02-13 14:22:25 -0300

    Se agregó el evento click de jquery en el botón enviar correoa traves de id, mostrando el mensaje por un alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$(document).ready(function(){
+    $("#enviarCorreo").click(function(){
+      alert("El correo fue enviado correctamente...");
+    });
+
+    
+  });
