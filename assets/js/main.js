15f339c391b1b6390f3ee05baabfef0dce1a62d8
magysil <magallysilvab@gmail.com>
2024-02-13 23:41:43 -0300

    Se le dió formato a los archivos, HTML, CSS y JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,30 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
-$(document).ready(function(){
+    $(document).ready(function(){
 
-    /*Evento “click” de jquery para mostrar una alerta al hacer un clicK
-sobre el botón de "Enviar por Correo" mediante el uso de los selectores por Id*/
-    $("#enviarCorreo").click(function(){
-      alert("El correo fue enviado correctamente...");
+        /*Evento “click” de jquery para mostrar una alerta al hacer un clicK
+    sobre el botón de "Enviar por Correo" mediante el uso de los selectores por Id*/
+        $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correctamente...");
+        });
+
+        /*Selectores de etiqueta y el método de 
+    `on` con el evento `dblclick`, para modificar el color del texto */
+
+        $("p.ingredientes").on("dblclick", function(){
+            $(this).css("color", "red");
+        });
+
+        $("p.preparacion").on("dblclick", function(){
+            $(this).css("color", "red");        
+        });
+
+        //Selector de Clase con el método toggle para  desaparecer y aparecer el contenido de las cards 
+
+        $(".card-title").click(function(){
+            $(".card-text").toggle();
+        });  
+
     });
-
-    /*Selectores de etiqueta y el método de 
-`on` con el evento `dblclick`, para modificar el color del texto */
-
-    $("p.ingredientes").on("dblclick", function(){
-        $(this).css("color", "red");
-    });
-
-    $("p.preparacion").on("dblclick", function(){
-        $(this).css("color", "red");        
-    });
-
-    //Selector de Clase con el método toggle para  desaparecer y aparecer el contenido de las cards 
-
-    $(".card-title").click(function(){
-        $(".card-text").toggle();
-      });  
-
-  });
